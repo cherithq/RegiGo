1895d0791f8438df956cdd3c23c44a6daf4c1e64
cherithq <cheritanhuiqi@gmail.com>
2026-07-25 20:10:27 +0800

    fix

--- components/layout/DashboardSidebar.tsx
+++ components/layout/DashboardSidebar.tsx
@@ -17,7 +17,7 @@ import type {
     SidebarModuleKey,
     SidebarModuleMap,
 } from "@/lib/sidebar-modules";
-import Logo from "@/components/layout/logo";
+import Logo from "@/components/layout/Logo";
 import {
     useSidebar,
 } from "./SidebarContext";
